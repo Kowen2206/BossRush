972551d201a44dbbae759dc156f11261280ff21e
Seafex <exo.1040gt@gmail.com>
2023-02-10 23:06:48 -0600

    Codigo fuente

--- BossRushJam/Assets/Scripts/F_Interface/StartGameButton/MenuToSettings.cs
+++ BossRushJam/Assets/Scripts/F_Interface/StartGameButton/MenuToSettings.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public class MenuToSettings : MonoBehaviour
 {
-   
-
+    private string [] TextOptions = new string[3];
+// Variables para el movimiento de la interfaz
     [Header("options")]
     public Slider VolumeFX;
     public Slider VolumeMusic;
@@ -14,20 +14,35 @@ public class MenuToSettings : MonoBehaviour
 
     [Header("menus")]
     public GameObject MainMenu;
-    public GameObject SettingsMenu;
+    public GameObject SettingsMen;
     public GameObject KeyBoardImage;
-     
+    
+    void Start()
+    {
+        MainMenu.SetActive(true);
+        SettingsMen.SetActive(false);
+        KeyBoardImage.SetActive(false);
+       
+
+    //Textos a lo random
+    TextOptions[0] = "Made in Mexico";
+    TextOptions[1] = "¡Lucha lucha!";
+    TextOptions[2] = "¡Rompe las cajas!";
+
+    
+
+
+        
+     }
 
      public void OpenPanel(GameObject Menu)
     {
-        SettingsMenu.SetActive(false);
         MainMenu.SetActive(false);
+        SettingsMen.SetActive(false);
         KeyBoardImage.SetActive(false);
-
-        
-        
-
+    
         Menu.SetActive(true);
     }
+   
  
 }
