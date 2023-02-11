10d982e68cb985145e07467d48894a471a8fc452
Seafex <exo.1040gt@gmail.com>
2023-02-11 17:03:14 -0600

    nada

--- BossRushJam/Assets/Scripts/F_Interface/MainScripts/RandomText.cs
+++ BossRushJam/Assets/Scripts/F_Interface/MainScripts/RandomText.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 
 public class  RandomText: MonoBehaviour
 {
     // Texto Random en la pantalla de inicio
+    
+
+
   
     
-        public void Text(GameObject Text)
-        {
-
-        }
+       
 }
