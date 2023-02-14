56721fcce9d39479e4d961bea32820c87f267c03
Seafex <exo.1040gt@gmail.com>
2023-02-14 00:24:51 -0600

    fadeout

--- BossRushJam/Assets/Scripts/F_Interface/MovemetMenus/MenuToSettings.cs
+++ BossRushJam/Assets/Scripts/F_Interface/MovemetMenus/MenuToSettings.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 public class MenuToSettings : MonoBehaviour
 {
+    public Animator animator;
+    public float SliderValue;
+    public Image ImagenMute;
 // Variables para el movimiento de la interfaz
     [Header("options")]
     public Slider VolumeMusic;
@@ -14,6 +17,7 @@ public class MenuToSettings : MonoBehaviour
     public GameObject MainMenu;
     public GameObject SettingsMenu;
     public GameObject KeyBoardImage;
+    public GameObject Fade;
 
     private void Start()
     {
@@ -21,6 +25,31 @@ public class MenuToSettings : MonoBehaviour
         SettingsMenu.SetActive(false);
         KeyBoardImage.SetActive(false);
         //Control de Audio
+        VolumeMusic.value = PlayerPrefs.GetFloat("Volumenaudio", 1f);
+        AudioListener.volume = VolumeMusic.value;
+        ImagenMute.enabled = false;
+        NomuteSearch();
+
+     }
+     private void ChangeSlider(float valor)
+     {
+        SliderValue = valor;
+        PlayerPrefs.SetFloat("Volumenaudio",SliderValue);
+        AudioListener.volume = VolumeMusic.value;
+        NomuteSearch();
+     }
+
+     private void NomuteSearch()
+     {
+        if (SliderValue == 0)
+        {
+            ImagenMute.enabled = true;
+        }
+        else
+        {
+            ImagenMute.enabled = false;
+        }
+
      }
     
 
@@ -29,8 +58,14 @@ public class MenuToSettings : MonoBehaviour
         MainMenu.SetActive(false);
         SettingsMenu.SetActive(false);
         KeyBoardImage.SetActive(false);
-    
+        FadeOut();
         Menu.SetActive(true);
+    }
+
+    private void FadeOut()
+    {
+        animator.Play("FadeOut");
+
     }
    
  
