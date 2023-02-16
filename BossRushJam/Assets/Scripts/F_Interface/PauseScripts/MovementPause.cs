d72a0e601df5e085e5fd551f5ea28ed324a7171a
Seafex <exo.1040gt@gmail.com>
2023-02-15 19:33:59 -0600

    avance4

--- BossRushJam/Assets/Scripts/F_Interface/PauseScripts/MovementPause.cs
+++ BossRushJam/Assets/Scripts/F_Interface/PauseScripts/MovementPause.cs
@@ -3,38 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
-using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class MovementPause : MonoBehaviour
-{ [Header("Pause")]
+{ 
+    [Header("Pause")]
     public GameObject PauseMen;
     public GameObject Settings1;
-    public AudioSource fuentes;
-    public AudioClip Clicks;
+    public GameObject Origins;
+    public AudioSource Fuente;
+    public AudioClip click;
     void start()
     {
-        StartCoroutine(PauseMenu());
-        fuentes.clip = Clicks;
-
-    }
-    void update()
-    {
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            PauseMen.SetActive(true);
-            Debug.Log ("ESC");
-        }
-    }
-    
-    IEnumerator PauseMenu()
-    {
-        yield return new WaitForSeconds(0.0f);
-        PauseMen.SetActive(false);
-        Settings1.SetActive(false);
+        Fuente.clip = click;
+        StartCoroutine(PauseMenus());
     }
     public void Resumen()
     {
-        PauseMen.SetActive(false);
+        Origins.SetActive(false);
+    }
+    public void Settings2()
+    {
+        Origins.SetActive(true);
         Settings1.SetActive(false);
     }
     public void settingsPause()
@@ -42,20 +32,20 @@ public class MovementPause : MonoBehaviour
         PauseMen.SetActive(false);
         Settings1.SetActive(true);
     }
-    public void Exit()
+    public void Exit1()
     {
         PauseMen.SetActive(true);
         Settings1.SetActive(false);
 
     }
-    public void Sonido0()
+    public void sonido()
     {
-        fuentes.Play();
+        Fuente.Play();
     }
-    public void Scene0()
+     IEnumerator PauseMenus()
     {
-        SceneManager.LoadScene(0);
+        yield return new WaitForSeconds(0f);
+        Origins.SetActive(false);
+        Settings1.SetActive(false);
     }
-    
-    
 }
